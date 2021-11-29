decb553298c6c253fbf8aa43653ab15e16773980
mehmetpekcan <pekcanmehmett@gmail.com>
2021-11-29 18:42:37 +0300

    Change order of package.json file

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,11 @@
-function pushWithI18n(router, name, options = {}) {
+export function pushWithI18n(router, name, options = {}) {
   // router.push("/", "/ahmet");
 
   console.log("pushWithI18n", router.getTime());
 }
 
-function replaceWithiI18n(router, name, options = {}) {
+export function replaceWithiI18n(router, name, options = {}) {
   // router.push("/", "/ahmet");
 
   console.log("replaceWithiI18n", router.getTime());
 }
-
-module.exports = {
-  pushWithI18n,
-  replaceWithiI18n,
-};
