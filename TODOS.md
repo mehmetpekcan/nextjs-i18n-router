8e1c688eccc9730a3fe3d7b165f5e288f0acbccf
mehmetpekcan <pekcanmehmett@gmail.com>
2021-12-30 23:09:33 +0300

    feature: add closeFileSystemRoute feature

--- TODOS.md
+++ TODOS.md
@@ -1,3 +1,4 @@
 [ ] Add query support while creating url for left params.
 [ ] Add route guard rules
 [ ] Add regex that converts destination to pathname
+[ ] Add warning to add **Page** suffix to routes when `closeFileSystemRoute` active to prevent wrong 404
--- src/core/withNextI18nRouter.js
+++ src/core/withNextI18nRouter.js
@@ -1,13 +1,62 @@
 const fs = require("fs");
 const path = require("path");
 
+/**
+ * Creates a bunch of 404 destinated routes list
+ * with the given pages
+ */
+const create404Routes = (pages) =>
+  pages.flatMap((page) => {
+    let source = `/${page.replace(".js", "")}`;
+    let destination = "/404";
+
+    if (source === "/index") {
+      return { source: "/", destination };
+    }
+
+    return [
+      { source, destination },
+      { source: `${source}/(.*)`, destination },
+    ];
+  });
+
+/**
+ * Returns the file names list on native NextJS pages folder
+ */
+const getPagesDirectoryRoutes = () => {
+  const RESERVED_PAGES = [
+    "_app.js",
+    "_document.js",
+    "_error.js",
+    "404.js",
+    "500.js",
+    "api",
+  ];
+  const PAGES_PATHS = ["pages", "src/pages"];
+  const pagesDirectory = PAGES_PATHS.find((dir) =>
+    fs.existsSync(path.join(process.cwd(), dir))
+  );
+
+  if (!pagesDirectory) {
+    throw new Error(
+      "No pages folder defined, please create your pages folder under `pages` or `src/pages` directory"
+    );
+  }
+
+  return fs
+    .readdirSync(path.join(process.cwd(), pagesDirectory))
+    .filter((page) => !RESERVED_PAGES.includes(page));
+};
+
+/**
+ * Parses the plugin config file and creates the routes
+ * according to given config manifest
+ */
 const parseConfigFile = ({ locales, defaultLocale }) => {
   try {
+    const routes = { afterFiles: {}, beforeFiles: [] };
     const configPath = path.join("i18n-routes.json");
-
     const routesJSON = fs.readFileSync(configPath, "utf8");
-
-    const routesTree = {};
     const { pages, translations, options } = JSON.parse(routesJSON);
 
     Object.entries(pages).forEach((entry) => {
@@ -20,10 +69,10 @@ const parseConfigFile = ({ locales, defaultLocale }) => {
           source = source.replace(key, translations[value][locale]);
         }
 
-        routesTree[name] = {
+        routes.afterFiles[name] = {
           ...restPageOptions,
           alternates: {
-            ...routesTree[name]?.alternates,
+            ...routes.afterFiles[name]?.alternates,
             [locale]: {
               source: `/${locale}${source}`,
               destination: `/${locale}${destination}`,
@@ -33,25 +82,26 @@ const parseConfigFile = ({ locales, defaultLocale }) => {
       });
     });
 
-    return {
-      routes: routesTree,
-      options: { ...options, defaultLocale: defaultLocale },
-    };
+    if (options.closeFileSystemRouting) {
+      const pagesDirectoryRoutes = getPagesDirectoryRoutes();
+      routes.beforeFiles = create404Routes(pagesDirectoryRoutes);
+    }
+
+    return { routes, options: { ...options, defaultLocale: defaultLocale } };
   } catch (err) {
     throw new Error(err);
   }
 };
 
+/**
+ * Adapts the given routes to the NextJS rewrites config formats
+ */
 const adaptRoutesToNextJS = (routes) => {
   const rewrites = [];
 
   Object.values(routes).forEach((route) => {
-    Object.values(route.alternates).forEach((page) => {
-      rewrites.push({
-        locale: false,
-        source: page.source,
-        destination: page.destination,
-      });
+    Object.values(route.alternates).forEach(({ source, destination }) => {
+      rewrites.push({ locale: false, source, destination });
     });
   });
 
@@ -63,23 +113,33 @@ const withNextI18nRouter = (nextConfig = {}) => {
 
   return Object.assign({}, nextConfig, {
     async rewrites() {
-      const i18nRewrites = adaptRoutesToNextJS(config.routes);
+      const i18nRewrites = adaptRoutesToNextJS(config.routes.afterFiles);
       const existingRewrites = nextConfig.rewrites
         ? await nextConfig.rewrites()
         : [];
 
       if (Array.isArray(existingRewrites)) {
-        return [...existingRewrites, ...i18nRewrites];
+        return {
+          beforeFiles: [...config.routes.beforeFiles],
+          afterFiles: [...existingRewrites, ...i18nRewrites],
+        };
       }
 
       return {
         ...existingRewrites,
+        beforeFiles: [
+          ...beforeFiles(existingRewrites.beforeFiles || []),
+          ...config.routes.beforeFiles,
+        ],
         afterFiles: [...(existingRewrites.afterFiles || []), ...i18nRewrites],
       };
     },
     publicRuntimeConfig: {
       ...nextConfig.publicRuntimeConfig,
-      nextI18nRoutes: JSON.stringify(config),
+      nextI18nRoutes: JSON.stringify({
+        ...config,
+        routes: config.routes.afterFiles,
+      }),
     },
   });
 };
